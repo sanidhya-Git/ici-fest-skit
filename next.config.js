8a9990475d10cb840e916228fc673e493c603067
sanidhya-Git <sanidhyamehra36@gmail.com>
2026-09-16 08:39:07 +0530

    up

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
 /** @type {import("next").NextConfig} */
 const config = {
-  transpilePackages: [
-    "uploadthing",
-    "@uploadthing/react",
-    "@uploadthing/shared",
-    "@uploadthing/mime-types",
-  ],
-  turbo: {
-    rules: {
-      "**/*.md": {
-        loaders: ["null-loader"],
-        as: "*.js",
-      },
-      "**/*.d.cts": {
-        loaders: ["null-loader"],
-        as: "*.js",
-      },
-    },
-  },
   images: {
     remotePatterns: [
       {
